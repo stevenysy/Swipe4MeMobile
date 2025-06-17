0bb2e3c543bf4f9adc47073221d27f3d6c286821
Steven Yi <stevenjxhc@gmail.com>
2025-06-16 22:50:45 -0500

    temp

--- Swipe4MeMobile/MasterView.swift
+++ Swipe4MeMobile/MasterView.swift
@@ -53,6 +53,7 @@ struct MasterView: View {
                 NavigationStack {
                     CreateSwipeRequestView(request: SwipeRequest())
                         .environment(authManager)
+//                    OpenRequestsView()
                 }
             }
         }
