facc53f87329da2e1af7ee06b6811b66b05bb9cb
Steven Yi <stevenjxhc@gmail.com>
2025-06-17 22:09:01 -0500

    implement microsoft sign in

--- Swipe4MeMobile/Authentication/LoginView.swift
+++ Swipe4MeMobile/Authentication/LoginView.swift
@@ -13,6 +13,8 @@ struct LoginView: View {
         Text("Welcome to Swipe4Me!")
         
         GoogleSignInButton
+        
+        MicrosoftSignInButton
     }
     
     var GoogleSignInButton: some View {
@@ -28,6 +30,29 @@ struct LoginView: View {
             HStack(alignment: .center, spacing: 0) {
                 Image("Google")
                 Text("Sign in with Google")
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .frame(height: 35)
+        }
+        .foregroundStyle(.primary)
+        .buttonStyle(.bordered)
+        .cornerRadius(10)
+    }
+    
+    var MicrosoftSignInButton: some View {
+        Button {
+            Task {
+                guard let user = await authManager.signInWithMicrosoft() else { return }
+                
+                if authManager.isFirstTimeSignIn {
+                    await UserManager.shared.createNewUser(newUser: user)
+                }
+            }
+        } label: {
+            HStack(alignment: .center, spacing: 0) {
+                Image("Microsoft")
+                Text("Sign in with Microsoft")
             }
             .padding()
             .frame(maxWidth: .infinity)
