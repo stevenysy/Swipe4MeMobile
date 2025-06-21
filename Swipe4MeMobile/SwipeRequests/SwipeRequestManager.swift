9064fd2200e649ff17c616931c6215c65b87deda
Steven Yi <stevenjxhc@gmail.com>
2025-06-20 22:28:49 -0500

    create deleteRequest func

--- Swipe4MeMobile/SwipeRequests/SwipeRequestManager.swift
+++ Swipe4MeMobile/SwipeRequests/SwipeRequestManager.swift
@@ -27,4 +27,22 @@ final class SwipeRequestManager {
             errorMessage = error.localizedDescription
         }
     }
+    
+    /// Deletes a request from Firestore and updates `errorMessage` on failure.
+    ///
+    /// - Parameter request: The `SwipeRequest` to remove.
+    func deleteRequest(_ request: SwipeRequest) {
+        guard let id = request.id else {
+            self.errorMessage = "Request has no valid document ID."
+            return
+        }
+
+        db.collection("swipeRequests").document(id).delete { error in
+            if let error = error {
+                self.errorMessage = error.localizedDescription   // <- Triggers alert
+            } else {
+                self.errorMessage = ""   // Clear any previous error
+            }
+        }
+    }
 }
